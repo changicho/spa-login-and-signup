c4e2150695760fc0cd57f920a4c9813927ec50a9
eunuch74 <eunuch74@gmail.com>
2019-09-05 16:52:18 +0900

    추가 : axios 라이브러리 추가
    axios 라이브러리 추가. npm으로 사용하지 않을 떄는 html 내에 script src=url로 사용 가능

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// low-db 추가
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,10 +19,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,17 +26,32 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* GET home page. */
-app.get('/', function (req, res, next) {
-  res.render('main');
+app.get('/', function (request, response, next) {
+  response.render('main');
 });
 
-app.get('/signup', function (req, res, next) {
-  res.render('signup');
+app.get('/signup', function (request, response, next) {
+  response.render('signup');
 });
 
-app.get('/login', function (req, res, next) {
-  res.render('login');
+app.get('/login', function (request, response, next) {
+  response.render('login');
 });
+
+app.post('/sendAccountInformation', function(request, response, next){
+  // console.log(request.body);
+  // request.body 는 json
+  push_account(requst.body);
+  response.render('login');
+})
+
+app.post('/checkData', function(request, response, next){
+  console.log(request.body);
+})
+
+app.post('/checkConfidentiality', function(request, response, next){
+  console.log(request.body);
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -54,5 +68,9 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+function push_account(reqiest_body){
+
+}
 
 module.exports = app;
