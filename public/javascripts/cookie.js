d56905a1da5dc3e0ce442ba14b62ea400133bae1
eunuch74 <eunuch74@gmail.com>
2019-09-09 16:04:15 +0900

    추가 : 세션 체크 후 오래된 세션 제거기능 추가
    app.js 에서 호출하는 api 라우터에서 setInterval 로 주기적으로 체크
    로그인 시 session table에 내용 추가
    expire time 이 지나면 session table 에서 제거

--- public/javascripts/cookie.js
+++ public/javascripts/cookie.js
@@ -5,16 +5,21 @@ let div_login_message = document.querySelector("#onlogin")
 if (cookies.length > 2) {
   div_login_message.style.display = "flex";
 
-  console.log(cookies)
-
   let p_user_name = document.querySelector("#user_name")
-  console.log(p_user_name)
-  let user_name = cookies[2].split("=")[1];
-  console.log(user_name)
+  let uuid = cookies[2].split("=")[1];
 
   navigation_menu_list[1].innerHTML = '<a onclick="toLogoutApi()">Logout</a>'
 
-  p_user_name.innerText = user_name
+  axios({
+    url: "api/get_username_by_uuid",
+    method: "post",
+    data: {
+      uuid : uuid
+    }
+  }).then(res => {
+    p_user_name.innerText = res.data.user_name;
+  });
+
 
 } else {
   navigation_menu_list[1].innerHTML = '<a href="/login">Login</a>'
@@ -26,12 +31,10 @@ function toLogoutApi(){
     url: "api/logout",
     method: "post",
     data: {
-      id : cookies[2].split("=")[1]
+      uuid : cookies[2].split("=")[1]
     }
   }).then(res => {
-    console.log(res);
     if (res.data) {
-      console.log('check pass!')
       location.href = "/login"
     }else{
       console.log('check fail!')
